5a337330e1db5037ef69abfcb430066b5ade6839
Olga Botvinnik <olga.botvinnik@gmail.com>
2020-11-29 16:44:03 -0800

    Add f-strings

--- bin/differential_hash_expression.py
+++ bin/differential_hash_expression.py
@@ -167,8 +167,8 @@ def main(metadata_csv, ksize, molecule, group_col=GROUP, group1=None, sig_col=SI
     if len(overlapping_samples) == 0:
         raise ValueError(
         f"Could not find any overlapping sample ids between the metadata and the sketch names!"
-        " \n--- 10 random sample ids from Metadata: ----\n{metadata.sample(10).index.values)}\n"
-        "\n--- 10 random sample ids from sketches: ---\n{sketch_series.sample(10).index.values)}"
+        f" \n--- 10 random sample ids from Metadata: ----\n{metadata.sample(10).index.values)}\n"
+        f"\n--- 10 random sample ids from sketches: ---\n{sketch_series.sample(10).index.values)}"
     )
 
     # If group1 is provided, only do one hash enrichment
